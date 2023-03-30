f7d7cd0adb21461bb2f0ba484235960d0d5ca117
Sangkon, Han <sigmadream@gmail.com>
2023-03-30 12:23:10 +0900

    Added 캘리포니아 데이터 R로 분석하는 방법을 RMarkdown 사용 (#3)
    - Kaggle 데이터 기반으로 R을 사용해서 분석하는 과정을 정리

---
+++ practice/practice03_california.R
@@ -0,0 +1,97 @@
+# 필수 라이브러리
+library(tidyverse)
+library(reshape2)
+library(boot)
+library(randomForest)
+
+housing = read.csv('./data/housing.csv')
+head(housing)
+summary(housing)
+
+## 시각화
+par(mfrow=c(2,5))
+colnames(housing)
+ggplot(data = melt(housing), mapping = aes(x = value)) + geom_histogram(bins = 30) + facet_wrap(~variable, scales = 'free_x')
+
+## 데이터 정리
+### NA 값
+housing$total_bedrooms[is.na(housing$total_bedrooms)] = median(housing$total_bedrooms , na.rm = TRUE)
+housing$mean_bedrooms = housing$total_bedrooms/housing$households
+housing$mean_rooms = housing$total_rooms/housing$households
+drops = c('total_bedrooms', 'total_rooms')
+housing = housing[ , !(names(housing) %in% drops)]
+head(housing)
+
+### 카테고리
+categories = unique(housing$ocean_proximity)
+cat_housing = data.frame(ocean_proximity = housing$ocean_proximity)
+
+for(cat in categories){
+  cat_housing[,cat] = rep(0, times= nrow(cat_housing))
+}
+head(cat_housing) #see the new columns on the right
+
+for(i in 1:length(cat_housing$ocean_proximity)){
+  cat = as.character(cat_housing$ocean_proximity[i])
+  cat_housing[,cat][i] = 1
+}
+head(cat_housing)
+
+cat_columns = names(cat_housing)
+keep_columns = cat_columns[cat_columns != 'ocean_proximity']
+cat_housing = select(cat_housing,one_of(keep_columns))
+tail(cat_housing)
+
+colnames(housing)
+drops = c('ocean_proximity','median_house_value')
+housing_num =  housing[ , !(names(housing) %in% drops)]
+head(housing_num)
+scaled_housing_num = scale(housing_num)
+head(scaled_housing_num)
+
+cleaned_housing = cbind(cat_housing, scaled_housing_num, median_house_value=housing$median_house_value)
+head(cleaned_housing)
+
+set.seed(42) # Set a random seed so that same sample can be reproduced in future runs
+sample = sample.int(n = nrow(cleaned_housing), size = floor(.8*nrow(cleaned_housing)), replace = F)
+train = cleaned_housing[sample, ] #just the samples
+test  = cleaned_housing[-sample, ] #everything but the samples
+
+head(train)
+nrow(train) + nrow(test) == nrow(cleaned_housing)
+
+glm_house = glm(median_house_value~median_income+mean_rooms+population, data=cleaned_housing)
+k_fold_cv_error = cv.glm(cleaned_housing , glm_house, K=5)
+k_fold_cv_error$delta
+
+glm_cv_rmse = sqrt(k_fold_cv_error$delta)[1]
+glm_cv_rmse #off by about $83,000... it is a start
+
+names(glm_house) #what parts of the model are callable?
+glm_house$coefficients
+
+names(train)
+set.seed(42)
+train_y = train[,'median_house_value']
+train_x = train[, names(train) !='median_house_value']
+head(train_y)
+head(train_x)
+
+rf_model = randomForest(train_x, y = train_y , ntree = 500, importance = TRUE)
+names(rf_model) #these are all the different things you can call from the model.
+rf_model$importance
+
+oob_prediction = predict(rf_model) #leaving out a data source forces OOB predictions
+train_mse = mean(as.numeric((oob_prediction - train_y)^2))
+oob_rmse = sqrt(train_mse)
+oob_rmse
+
+test_y = test[,'median_house_value']
+test_x = test[, names(test) !='median_house_value']
+
+y_pred = predict(rf_model , test_x)
+test_mse = mean(((y_pred - test_y)^2))
+test_rmse = sqrt(test_mse)
+test_rmse
+
+
---
+++ rnotebook/note03-2.R
@@ -0,0 +1,295 @@
+install.packages("readr")
+library(readr)
+install.packages("ggplot2")
+library(ggplot2)
+install.packages("knitr")
+library(knitr)
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("caret")
+library(caret)
+install.packages("leaps")
+library(leaps)
+install.packages("car")
+library(car)
+install.packages("mice")
+library(mice)
+install.packages("scales")
+library(scales)
+install.packages("RColorBrewer")
+library(RColorBrewer)
+install.packages("plotly")
+library(plotly)
+install.packages("nortest")
+library(nortest)
+install.packages("lmtest")
+library(lmtest)
+
+# 데이터 읽어오기
+housing_data <- read_csv("./data/housing.csv")
+summary(housing_data)
+
+# 시각화(지도는 아니지만, 지도 같은 시각화)
+plot_map = ggplot(housing_data, 
+                  aes(x = longitude, y = latitude, color = median_house_value, 
+                      hma = housing_median_age, tr = total_rooms, tb = total_bedrooms,
+                      hh = households, mi = median_income)) +
+  geom_point(aes(size = population), alpha = 0.4) +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  ggtitle("Data Map - Longtitude vs Latitude and Associated Variables") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_color_distiller(palette = "Paired", labels = comma) +
+  labs(color = "Median House Value (in $USD)", size = "Population")
+plot_map
+
+# 전처리
+
+## 이걸 어쩐다?
+housing_data$ocean_proximity <- as.factor(housing_data$ocean_proximity)
+levels(housing_data$ocean_proximity)
+ggplot(housing_data, aes(x = factor(ocean_proximity))) +
+  geom_bar(stat = "count", color = "black", fill = "blue")
+
+## 지우자!
+summary(housing_data$ocean_proximity)
+housing_data <- housing_data[housing_data$ocean_proximity != "ISLAND", ]
+
+## NA 처리
+sum(is.na(housing_data))
+total_bedrooms <- housing_data$total_bedrooms
+sum(is.na(total_bedrooms))
+
+### 중앙 vs 중위
+bedroom_mean = mean(housing_data$total_bedrooms, na.rm=TRUE)
+bedroom_median = median(housing_data$total_bedrooms, na.rm=TRUE)
+ggplot(housing_data, aes(x = total_bedrooms)) +
+  geom_histogram(bins = 40, color = "black", fill = "blue") +
+  geom_vline(aes(xintercept = bedroom_mean, color = "Mean"), lwd = 1.5) +
+  geom_vline(aes(xintercept = bedroom_median, color = "Median"), lwd = 1.5) +
+  xlab("Total Bedrooms") +
+  ylab("Frequency") +
+  ggtitle("Histogram of Total Bedrooms (noncontinuous variable)") +
+  scale_color_manual(name = "Summary Stats", labels = c("Mean", "Median"), values = c("red", "green"))
+housing_data$total_bedrooms[is.na(housing_data$total_bedrooms)] <- bedroom_median
+
+### 데이터 시각화
+str(housing_data)
+par(mfrow = c(3, 3))
+hist(housing_data$longitude, breaks = 20, main = "longitude", border="darkorange", col="dodgerblue")
+hist(housing_data$latitude, breaks = 20, main = "latitude", border="darkorange", col="dodgerblue")
+hist(housing_data$housing_median_age, breaks = 20, main = "housing_median_age", border="darkorange", col="dodgerblue")
+hist(housing_data$total_rooms, breaks = 20, main = "total_rooms", border="darkorange", col="dodgerblue")
+hist(housing_data$total_bedrooms, breaks = 20, main = "total_bedrooms", border="darkorange", col="dodgerblue")
+hist(housing_data$population, breaks = 20, main = "population", border="darkorange", col="dodgerblue")
+hist(housing_data$households, breaks = 20, main = "households", border="darkorange", col="dodgerblue")
+hist(housing_data$median_income, breaks = 20, main = "median_income", border="darkorange", col="dodgerblue")
+hist(housing_data$median_house_value, breaks = 20, main = "median_house_value", border="darkorange", col="dodgerblue")
+
+### 후처리가 끝났다고 가정한다면 이 영원할 것 같은 함수도 사용 가능
+par(mfrow = c(1, 1))
+pairs(housing_data, col = "dodgerblue")
+
+### 상관계수를 확인
+housing_data_nc <- housing_data[, -10]
+corrmatrix <- cor(housing_data_nc)
+kable(t(corrmatrix))
+
+# 머신러닝
+## 데이터 분류
+set.seed(42)
+housing_trn_idx <- createDataPartition(housing_data$ocean_proximity, p = .70, list = FALSE)
+housing_trn_data <- housing_data[housing_trn_idx, ]
+housing_tst_data <- housing_data[-housing_trn_idx, ]
+
+## 
+full_additive_model = lm(median_house_value ~ ., data = housing_trn_data)
+full_additive_adjr2 = summary(full_additive_model)$adj.r.squared
+full_twoway_model = lm(median_house_value ~ (.)^2, data = housing_trn_data)
+full_twoway_adjr2 = summary(full_twoway_model)$adj.r.squared
+full_threeway_model = lm(median_house_value ~ (.)^3, data = housing_trn_data)
+full_threeway_adjr2 = summary(full_threeway_model)$adj.r.squared
+
+beginning_mods_results = data.frame(
+  "Total Predictors" =
+    c("Additive Model" = extractAIC(full_additive_model)[1],
+      "Two-Way Int. Model" = extractAIC(full_twoway_model)[1],
+      "Three-Way Int. Model" = extractAIC(full_threeway_model)[1]),
+  "AIC" =
+    c("Additive Model" = extractAIC(full_additive_model)[2],
+      "Two-Way Int. Model" = extractAIC(full_twoway_model)[2],
+      "Three-Way Int. Model" = extractAIC(full_threeway_model)[2]),
+  "Adj R-Squared" =
+    c("Additive Model" = full_additive_adjr2,
+      "Two-Way Int. Model" = full_twoway_adjr2,
+      "Three-Way Int. Model" = full_threeway_adjr2))
+kable(beginning_mods_results, align = c("c", "r"))
+
+back_additive_mod_finish_aic = step(full_additive_model, direction = "backward", trace = 0)
+both_additive_mod_finish_aic = step(full_additive_model, direction = "both", trace = 0)
+n = length(resid(full_additive_model))
+back_additive_mod_finish_bic = step(full_additive_model, direction = "backward", k = log(n), trace = 0)
+both_additive_mod_finish_bic = step(full_additive_model, direction = "both", k = log(n), trace = 0)
+back_twoway_mod_finish_aic = step(full_twoway_model, direction = "backward", trace = 0)
+both_twoway_mod_finish_aic = step(full_twoway_model, direction = "both", trace = 0)
+n = length(resid(full_twoway_model))
+back_twoway_mod_finish_bic = step(full_twoway_model, direction = "backward", k = log(n), trace = 0)
+both_twoway_mod_finish_bic = step(full_twoway_model, direction = "both", k = log(n), trace = 0)
+
+aic_and_bic_results = data.frame(
+  "AIC" =
+    c("Backward" =
+        c("Additive" = extractAIC(back_additive_mod_finish_aic)[2],
+          "Two-Way" = extractAIC(back_twoway_mod_finish_aic)[2],
+          "Three-way" = extractAIC(full_threeway_model)[2]),
+      "Both" =
+        c("Additive" = extractAIC(both_additive_mod_finish_aic)[2],
+          "Two-Way" = extractAIC(both_twoway_mod_finish_aic)[2],
+          "Three-way" = extractAIC(full_threeway_model)[2])),
+  "BIC" =
+    c("Backward" =
+        c("Additive" = extractAIC(back_additive_mod_finish_bic)[2],
+          "Two-Way" = extractAIC(back_twoway_mod_finish_bic)[2],
+          "Three-way" = extractAIC(full_threeway_model)[2]),
+      "Both" =
+        c("Additive" = extractAIC(both_additive_mod_finish_bic)[2],
+          "Two-Way" = extractAIC(both_twoway_mod_finish_bic)[2],
+          "Three-way" = extractAIC(full_threeway_model)[2])))
+kable(aic_and_bic_results)
+
+diagnostics = function(model, alpha = .05, pointcol = "orange", linecol = "blue", plots = TRUE, tests = TRUE, pointtype = 16) {
+  if (plots == TRUE) {
+    par(mfrow = c(1, 3))
+    plot(
+      fitted(model),
+      resid(model),
+      pch = pointtype,
+      xlab = "Fitted Values",
+      ylab = "Residuals",
+      main = "Fitted vs Residuals",
+      col = pointcol
+    )
+    abline(h = 0, lwd = 2, col = linecol)
+    
+    qqnorm(
+      resid(model),
+      pch = pointtype,
+      main = "QQNorm Plot",
+      col = pointcol
+    )
+    qqline(
+      resid(model),
+      lwd = 2,
+      col = linecol
+    )
+    hist(
+      resid(model),
+      main = "Histogram of Residuals",
+      col = pointcol,
+      xlab = "Residuals",
+      ylab = "Frequency"
+    )
+  }
+  if (tests == TRUE) {
+    ad_test = ad.test(resid(model))
+    bp_test = bptest(model)
+    test_results = data.frame(
+      "Anderson-Darling Normality Test" =
+        c("Test Statistic" = round(ad_test$statistic, 5),
+          "P-Value" = ad_test$p.value,
+          "Result" = ifelse(ad_test$p.value < alpha, "Reject", "Fail To Reject")),
+      "Breusch-Pagan Test" =
+        c("Test Statistic" = round(bp_test$statistic, 5),
+          "P-Value" = bp_test$p.value,
+          "Result" = ifelse(bp_test$p.value < alpha, "Reject", "Fail To Reject")))
+    
+    kable(t(test_results), col.names = c("Test Statistic", "P-Value", "Decision"))
+  }
+}
+
+diagnostics(back_additive_mod_finish_aic)
+
+diagnostics(back_twoway_mod_finish_aic)
+
+diagnostics(full_threeway_model)
+
+back_twoway_mod_finish_aic_log = lm(formula = log(median_house_value) ~ longitude + latitude + housing_median_age + 
+                                      total_rooms + total_bedrooms + population + households + 
+                                      median_income + ocean_proximity + longitude:latitude + longitude:housing_median_age + 
+                                      longitude:total_rooms + longitude:total_bedrooms + longitude:households + 
+                                      longitude:median_income + longitude:ocean_proximity + latitude:housing_median_age + 
+                                      latitude:total_rooms + latitude:total_bedrooms + latitude:median_income + 
+                                      latitude:ocean_proximity + housing_median_age:total_rooms + 
+                                      housing_median_age:population + housing_median_age:households + 
+                                      housing_median_age:median_income + housing_median_age:ocean_proximity + 
+                                      total_rooms:population + total_rooms:households + total_rooms:median_income + 
+                                      total_rooms:ocean_proximity + total_bedrooms:households + 
+                                      total_bedrooms:median_income + total_bedrooms:ocean_proximity + 
+                                      population:households + population:median_income + population:ocean_proximity + 
+                                      households:median_income + households:ocean_proximity + median_income:ocean_proximity, 
+                                    data = housing_trn_data)
+
+diagnostics(back_twoway_mod_finish_aic_log)
+
+# From the text: http://daviddalpiaz.github.io/appliedstats/variable-selection-and-model-building.html
+calc_loocv_rmse = function(model) {
+  sqrt(mean((resid(model) / (1 - hatvalues(model))) ^ 2))
+}
+calc_rmse = function(actual, predicted) {
+  sqrt(sum((actual - predicted)^2) / length(actual)) 
+}
+calc_avg_per_error = function(actual, predicted) {
+  inter_abs = abs(predicted - actual)
+  100 * (sum(inter_abs / actual)) / length(actual)
+}
+
+additive_loocv_rmse = calc_loocv_rmse(back_additive_mod_finish_aic)
+twoway_loocv_rmse = calc_loocv_rmse(back_twoway_mod_finish_aic)
+threeway_loocv_rmse = calc_loocv_rmse(full_threeway_model)
+
+loocv_rmse_results = data.frame(
+  "LOOCV-RMSE" =
+    c("Backwards Additive" = additive_loocv_rmse,
+      "Backwards Two-Way" = twoway_loocv_rmse,
+      "Initial Three-way" = threeway_loocv_rmse))
+
+kable(loocv_rmse_results)
+
+# the actual median house values from the test set
+test_actual = housing_tst_data$median_house_value
+# the predicted house values for the test set
+test_predictions = predict(back_twoway_mod_finish_aic, housing_tst_data)
+# the RMSE
+test_rmse = calc_rmse(test_actual, test_predictions)
+# the percentage error
+test_perc_error = calc_avg_per_error(test_actual, test_predictions)
+
+test_rmse
+
+test_perc_error
+
+op0 = par()
+op1 = op0$mar
+op1[2] = 7
+par(mar = op1)
+plot(test_predictions - test_actual, housing_tst_data$ocean_proximity,
+     xlab = "Predicted Price Over / Under Actual Price",
+     ylab = "",
+     main = "Predicted Price Over / Under Actual Price vs Ocean Proximity",
+     col = "dodgerblue", yaxt = "none")
+axis(2, at = 1:5, labels = levels(housing_tst_data$ocean_proximity), las = 1)
+
+plot_map = ggplot(housing_tst_data, 
+                  aes(x = longitude, y = latitude, 
+                      color = test_predictions - test_actual, 
+                      hma = housing_median_age, tr = total_rooms, tb = total_bedrooms,
+                      hh = households, mi = median_income)) +
+  geom_point(aes(size = abs(test_predictions - test_actual)), alpha = 0.4) +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  ggtitle("Predicted Price Over / Under Actual Price") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_color_distiller(palette = "Paired", labels = comma) +
+  labs(color = "Predicted Price Over / Under (in $USD)", 
+       size = "Magnitude of Price Difference")
+plot_map
